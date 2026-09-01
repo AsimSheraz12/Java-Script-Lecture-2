7176007956699766d57f691d60554745b5cb1719
AsimSheraz12 <1240100471@uskt.edu.pk>
2026-09-01 11:23:30 +0500

    Add Comment Arthemetic Operators & Unary Operators File

---
+++ Lecture2.js
@@ -0,0 +1,77 @@
+// This forward symbol is used for Single line comments
+
+// This " /* */ " symbol is used for Multi-line comments
+
+//Arthmetic Operators Practice
+
+let a = 10, b = 5, c;
+
+console.log("Value of a is: " + a);
+console.log("Value of b is: " + b);
+
+//Addition Method 1
+c = a + b;
+console.log("\nAddition of a and b is: " + c + "\n");
+
+//Addition Method 2
+
+console.log("Addition of a and b is: " + (a + b) + "\n");
+
+//Subtraction Method 1
+c = a - b;
+console.log("Subtraction of a and b is: " + c + "\n");
+
+//Subtraction Method 2
+
+console.log("Subtraction of a and b is: " + (a - b) + "\n");
+
+//Multiplication Method 1
+c = a * b;
+console.log("Multiplication of a and b is: " + c + "\n");
+
+//Multiplication Method 2
+
+console.log("Multiplication of a and b is: " + (a * b) + "\n");
+
+//Division Method 1
+c = a / b;
+console.log("Division of a and b is: " + c + "\n");
+
+//Division Method 2
+
+console.log("Division of a and b is: " + (a / b) + "\n");
+
+//Modulus Method 1
+c = a % b;
+console.log("Modulus of a and b is: " + c + "\n");
+
+//Modulus Method 2
+
+console.log("Modulus of a and b is: " + (a % b) + "\n");
+
+
+// Unary Operators Practice
+
+let x = 10, y = 5;
+
+console.log("Value of x is: " + ++x); 
+
+/* ++x means pre-increment, it will increment the value of x by 1 and then return the value of x. */
+
+console.log("\n"); // Move Next line
+
+console.log("Value of y is: " + y++);
+
+/* y++ means post-increment, it will return the value of y and then increment the value of y by 1. */
+
+console.log("\n"); // Move Next line
+
+console.log("Value of x is: " + --x); 
+
+/* --x means pre-decrement, it will decrement the value of x by 1 and then return the value of x. */
+
+console.log("\n"); // Move Next line
+
+console.log("Value of y is: " + y--);
+
+/* y-- means post-decrement, it will return the value of y and then decrement the value of y by 1. */
